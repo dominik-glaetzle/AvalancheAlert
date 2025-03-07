cc533a13d300a496fd337ee0df1b2be4db858ee6
Dominik Glätzle <dominik@glaetzle.io>
2025-03-07 14:07:54 +0100

    added animated background

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Page from './components/Page.tsx';
+import { AnimatedBackground } from 'animated-backgrounds';
 
 function App() {
     return (
         <>
+            <AnimatedBackground animationName={'cosmicDust'} />
             <Page />
         </>
     );
