c34efd36476528f6782b1d34e6c59a837fecfd50
Dominik Glätzle <dominik@glaetzle.io>
2025-04-11 19:50:30 +0200

    added register + send verification email function

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -1,46 +1,48 @@
-import { Client, Databases, ID } from 'appwrite';
+import { Account, Client, Databases, ID } from 'appwrite';
+import { User } from './DTO/UserDTO';
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
-
-
+const VERIFICATION_URL = import.meta.env.VITE_APPWRITE_VERIFICATION_URL;
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject(PROJECT_ID);
 
 const database = new Databases(client);
-
-interface SubscriptionData {
-    email?: string;
-    phone?: string;
-    regions: string[];
-}
+const account = new Account(client);
 
 /**
- * Save new subscription to appwrite database
+ * create appwrite user and save new subscription to appwrite database
  */
-export const saveSubscription = async (data: SubscriptionData) => {
-    const { email, phone, regions } = data;
-
-    if (!email && !phone) {
-        throw new Error('Please provide at least an email or phone number.');
-    }
+export const createUser = async (user: User) => {
+    const { firstname, lastname, password, email, phone, regions } = user;
 
     try {
-            await database.createDocument(
-                DATABASE_ID,
-                COLLECTION_ID,
-                ID.unique(),
-                {
-                    email,
-                    phone,
-                    regions,
-                }
-            );
+        // create new appwrite user
+        const newUser = await account.create(ID.unique(), email, password);
+
+        // await account.createEmailPasswordSession(email, password!);
+
+        // send verification email -> TODO: rework email template
+        await account.createVerification(VERIFICATION_URL);
+        // create full database entry
+        await database.createDocument(
+            DATABASE_ID,
+            COLLECTION_ID,
+            ID.unique(),
+            {
+                userId: newUser.$id,
+                firstname,
+                lastname,
+                phone,
+                regions,
+            }
+        );
+        alert("Account created! Please verify your email.");
     } catch (error: any) {
-        console.error('❌ Error saving subscription:', error.message || error);
+        console.error('❌ Error creating subscription:', error.message || error);
         alert(error.message || error);
         throw error;
     }
