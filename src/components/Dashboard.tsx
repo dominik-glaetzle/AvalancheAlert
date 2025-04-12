a25e9ece37ee8a3631b576b9415dbb7946650a53
Dominik Glätzle <dominik@glaetzle.io>
2025-04-12 15:48:10 +0200

    added first basic dashboard components

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,7 @@
+import { Card } from '@mui/material';
+
 function Dashboard() {
-    return <div>Dashboard</div>;
+    return <Card variant="outlined" className={'bg-amber-50 w-70 h-160 mt-40 ml-40'} />;
 }
 
 export default Dashboard;
---
+++ src/components/DashboardPage.tsx
@@ -0,0 +1,7 @@
+import Sidebar from './Sidebar.tsx';
+
+function DashboardPage() {
+    return <Sidebar />;
+}
+
+export default DashboardPage;
