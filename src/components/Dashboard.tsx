3391be77f53000a567ef4532a0374923ab49bdba
Dominik Glätzle <dominik@glaetzle.io>
2025-04-17 19:46:51 +0200

    renamed dashboard component

--- src/components/Dashboard.tsx
+++
@@ -1,7 +0,0 @@
-import { Card } from '@mui/material';
-
-function Dashboard() {
-    return <Card variant="outlined" className={'bg-amber-50 w-70 h-160 mt-40 ml-40'} />;
-}
-
-export default Dashboard;
