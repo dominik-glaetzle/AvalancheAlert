05710cda1117fa549e9dbf5b83358287d9b4d3fe
Dominik Glätzle <dominik@glaetzle.io>
2025-04-17 19:44:45 +0200

    added alert card components

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,7 +1,31 @@
 import Sidebar from './Sidebar.tsx';
+import AlertCard from './AlertCard.tsx';
+import { Box } from '@mui/material';
 
-function DashboardPage() {
-    return <Sidebar />;
+export default function DashboardPage() {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                minHeight: '100vh',
+                padding: 2,
+                gap: 2,
+            }}
+        >
+            <Sidebar />
+
+            <Box
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+                    gap: 3,
+                    flexGrow: 1,
+                }}
+            >
+                <AlertCard name={'Wetter'} />
+                <AlertCard name={'Schneehöhe'} />
+                <AlertCard name={'Lawinengefahr'} />
+            </Box>
+        </Box>
+    );
 }
-
-export default DashboardPage;
